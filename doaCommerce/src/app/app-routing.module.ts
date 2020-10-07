811180a38172a2cc79a4698128e87b0d4464a4ab
unknown <portocrisel@hotmail.com>
2020-10-07 17:41:31 -0300

    Criação do produto-categoria

--- doaCommerce/src/app/app-routing.module.ts
+++ doaCommerce/src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { CadastroComponent } from './cadastro/cadastro.component';
 import { ContatoComponent } from './contato/contato.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { ProdutoCategoriaComponent } from './produto-categoria/produto-categoria.component';
+import { ProdutoComponent } from './produto/produto.component';
 import { SaibaMaisComponent } from './saiba-mais/saiba-mais.component';
 
 const routes: Routes = [
@@ -12,7 +14,9 @@ const routes: Routes = [
   { path: 'contato', component: ContatoComponent},
   { path: 'saiba-mais', component: SaibaMaisComponent},
   { path: 'login', component: LoginComponent},
-  { path: 'cadastro', component: CadastroComponent}
+  { path: 'cadastro', component: CadastroComponent},
+  { path: 'produtos', component: ProdutoComponent},
+  { path: 'produto-categoria', component: ProdutoCategoriaComponent}
 ];
 
 @NgModule({
